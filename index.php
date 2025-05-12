c4267febf751b0c623b60522c15f471e5da17d32
Gleidson <gleidsonnq@gmail.com>
2025-05-12 17:58:02 -0300

    paginaInicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="witdh=device-width, initial-scale=1.0">
+        <title> Dr Tec</title>
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+
+</html>
